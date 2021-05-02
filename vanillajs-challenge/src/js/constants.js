37f0dfc8b5a0ab634e05146855565a3c2561aa77
유성실 <yooo1201@naver.com>
2021-05-02 23:44:21 +0900

    add: todo 추가, finished 연동, UI

--- vanillajs-challenge/src/js/constants.js
+++ vanillajs-challenge/src/js/constants.js
@@ -2,4 +2,24 @@ function getRandomNumber(maxNumber) {
   return Math.floor(Math.random() * maxNumber);
 }
 
-export { getRandomNumber };
+function getLocalStorage(key) {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch {
+    return null;
+  }
+}
+
+function setLocalStorage(key, value) {
+  try {
+    return localStorage.setItem(key, JSON.stringify(value));
+  } catch {
+    return null;
+  }
+}
+
+function getTimeNow() {
+  return new Date().getTime();
+}
+
+export { getRandomNumber, getLocalStorage, setLocalStorage, getTimeNow };
--- vanillajs-challenge/src/js/index.js
+++ vanillajs-challenge/src/js/index.js
@@ -1,8 +1,119 @@
-import { getRandomNumber } from './constants.js';
+import { getLocalStorage, getRandomNumber, setLocalStorage, getTimeNow } from './constants.js';
 import { clock } from './clock.js';
 import { IMAGE_MAX_NUMBER, paintImage } from './bg.js';
 
 const $body = document.querySelector("body");
+const $nameForm = document.querySelector("#name-form");
+const $nameInput = $nameForm.querySelector("#name-input");
+const $userName = document.querySelector(".user-name");
+const $todoForm = document.querySelector("#todo-form");
+const $todoInput = $todoForm.querySelector("#todo-input");
+const $todoList = $todoForm.querySelector(".todo-wrapper");
+
+let todos = [];
+
+const NAME = 'name';
+const TODOS = 'todos';
+
+function handleNameSubmit(event) {
+  event.preventDefault();
+  const currentValue = $nameInput.value;
+  paintName(currentValue);
+  $nameInput.value = "";
+}
+
+function paintName(value) {
+  $userName.innerText = value;
+  setLocalStorage(NAME, value);
+  hideNameForm();
+  showTodoList();
+}
+
+function showNameForm() {
+  $nameForm.style.display = 'block';
+}
+
+function hideNameForm() {
+  $nameForm.style.display = "none";
+}
+
+function handleLoad() {
+  const loadedName = getLocalStorage(NAME);
+  if (loadedName) {
+    loadName(loadedName);
+    hideNameForm();
+    showTodoList();
+    loadTodos();
+  } else {
+    showNameForm();
+  }
+}
+
+function loadName(value) {
+  $userName.innerText = value;
+}
+
+function showTodoList() {
+  $todoForm.style.display = 'block';
+}
+
+function loadTodos() {
+  const loadedTodos = getLocalStorage(TODOS);
+  if (loadedTodos) {
+    const sortedArray = loadedTodos.sort((a,b) => a.finished - b.finished);
+    sortedArray.forEach(function ({id, value, finished}) {
+      paintTodo(value, id, finished);
+    });
+  }
+}
+
+function handleTodoSubmit(event) {
+  event.preventDefault();
+  const currentValue = $todoInput.value;
+  paintTodo(currentValue);
+  $todoInput.value = "";
+}
+
+function paintTodo(value, id, finished) {
+  const li = document.createElement("li");
+  li.innerHTML = `
+    <button type="button" class="check-button ${finished && 'checked'}"></button>
+    ${finished ? '<del>' + value + '</del>' : value}
+    <button type="button" class="delete-button"></button>
+  `;
+  const itemId = id ? id : getTimeNow();
+  li.id = itemId;
+
+  if (finished) {
+    $todoList.appendChild(li);
+  } else {
+    $todoList.prepend(li);
+  }
+
+  const todoItem = {
+    id: itemId,
+    value,
+    finished: finished || false
+  };
+
+  todos.push(todoItem);
+  setLocalStorage(TODOS, todos);
+}
+
+function handleChangeFinished(todoItem) {
+  const { id, value, finished } = todos.find(item => {return item.id === parseInt(todoItem.id)}) || {};
+  handleDeleteTodo(todoItem);
+  paintTodo(value, id, !finished);
+}
+
+function handleDeleteTodo(todoItem) {
+  todoItem.remove();
+  const newTodos = todos.filter(item => {
+    return item.id !== parseInt(todoItem.id)
+  });
+  todos = newTodos;
+  setLocalStorage(TODOS, todos);
+}
 
 function init() {
   clock();
@@ -10,6 +121,29 @@ function init() {
 
   const randomNumber = getRandomNumber(IMAGE_MAX_NUMBER);
   paintImage($body, randomNumber);
+
+  handleLoad();
+
+  $nameForm.addEventListener("submit", handleNameSubmit);
+  $todoForm.addEventListener("submit", handleTodoSubmit);
+
+  $todoList.addEventListener("click", function(event) {
+    const className = event.target.className;
+
+    if (className.includes('check-button')) {
+      if (className.includes('checked')) {
+        const $li = event.target.parentElement;
+        handleChangeFinished($li);
+      } else {
+        const $li = event.target.parentElement;
+        handleChangeFinished($li);
+      }
+    }
+    if (className === 'delete-button') {
+      const $li = event.target.parentElement;
+      handleDeleteTodo($li);
+    }
+  });
 }
 
 init();
